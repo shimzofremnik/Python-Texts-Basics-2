3face6edb32b3ecc8752ccdb11045e52ee727dc7
shimzofremnik <98681891+shimzofremnik@users.noreply.github.com>
2023-01-11 21:41:44 +0100

    Add files via upload

---
+++ sringbasics2.py
@@ -0,0 +1,39 @@
+# Task 1
+
+text = "Berlin is a world city of culture, politics, media and science."
+
+print(len(text))
+
+# Task 2
+
+print(text[0], text[-1])
+
+# Task 3
+
+print(text[0:3].upper())
+
+# Task 4
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+print("B appears", text.count("B"), "times.")
+
+# Task 5
+
+text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+
+print(text[-10:])
+
+# Task 6
+
+text = "---Python programming---"
+
+print(text.replace("-", ""))
+
+# Task 7
+
+first_name = "Mary"
+last_name = "Mat"
+
+print("Firstname:", first_name, "\nLastname:", last_name)
+
